9e5be2b86b84adbc0b1f811cb29fd36afea3d926
austinb97 <31427546+austinb97@users.noreply.github.com>
2018-11-30 19:07:19 -0700

    Add files via upload

--- js/pokecode.js
+++ js/pokecode.js
@@ -23,8 +23,17 @@ pokemon.forEach(elm => {
 	fig.appendChild(img);
 	fig.appendChild(cap);
 
+	let backName = document.createElement('h1');
+	backName.classList.add('backName');
+	backName.textContent = elm.ename;
+	cardBack.appendChild(backName);
+	let statHead = document.createElement('h2');
+	statHead.classList.add('statHead');
+	statHead.textContent = "Stats";
+	// cardBack.appendChild(statHead);
 	let statContainer = document.createElement('div');
 	statContainer.classList.add('statContainer');
+	statContainer.appendChild(statHead);
 	var statsArray = elm.base;
 	for(var stat in statsArray){
 		var p = document.createElement('p');
@@ -39,10 +48,58 @@ pokemon.forEach(elm => {
 
 });
 
-
+// FLIP CARDS
 document.getElementById('container').addEventListener("mouseover", function(e) {
 		if(e.target && e.target.matches('.card')){
 		  console.log('flip');
 		  e.target.classList.toggle('flip');
 		}
 	});
+
+
+// ADD CARD FUNCTIONALITY
+let btn = document.getElementById('addCardBtn').addEventListener("click", function(){
+	let imgName = `133Eevee.png`;
+	let card = document.createElement('div');
+	card.className = "card";
+	let cardFront = document.createElement('div');
+	cardFront.className = "cardFace cardFront";
+	let cardBack = document.createElement('div');
+	cardBack.className = "cardFace cardBack";
+	card.appendChild(cardFront);
+	card.appendChild(cardBack);
+	let fig = document.createElement('fig');
+	let cap = document.createElement('figcaption');
+	let img = document.createElement('img');
+	img.src = `img/${imgName}`;
+	cap.textContent = "Eevee";
+	cardFront.appendChild(fig);
+	fig.appendChild(img);
+	fig.appendChild(cap);
+
+	let backName = document.createElement('h1');
+	backName.classList.add('backName');
+	backName.textContent = "Eevee";
+	cardBack.appendChild(backName);
+	let statHead = document.createElement('h2');
+	statHead.classList.add('statHead');
+	statHead.textContent = "Stats";
+	// cardBack.appendChild(statHead);
+	let statContainer = document.createElement('div');
+	statContainer.classList.add('statContainer');
+	statContainer.appendChild(statHead);
+	var statsArray = {"Attack": 55, "Defense": 50, "HP": 55, "Sp.Atk": 45, "Sp.Def": 65, "Speed": 55};
+	for(var stat in statsArray){
+		var p = document.createElement('p');
+		p.classList.add('stat');
+		p.textContent = stat+": "+statsArray[stat];
+		console.log(p);
+		statContainer.appendChild(p);
+	}
+	cardBack.appendChild(statContainer);
+
+	pokeContainer.appendChild(card);
+});
+
+
+
